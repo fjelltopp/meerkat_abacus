833d676e457c6482e5e418fe79f40e2040a24433
Gunnar Ro <gunnarroe@gmail.com>
2015-10-27 10:56:22 +0000

    country-specific tables and making manage.py using commandline arguments

--- meerkat_abacus/manage.py
+++ meerkat_abacus/manage.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
+#!/usr/bin/python3
+import argparse
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from database_util.import_locations import import_regions
 from database_util.import_locations import import_clinics
@@ -10,21 +13,41 @@ from config import DATABASE_URL, country_config
 
 data_directory = "../data/"
 
-def create_db(url, base):
+
+def create_db(url, base,country_config, drop=False):
     """
     The function creates and sets up the datbase.
 
     Args:
     base: An SQLAlchmey declarative base with the db schema
     url : the database_url
+    country_config: A contry config dictionary
+    drop: Flag to drop the database before setting it up
 
     Returns:
     True
-
     """
+    if drop:
+        drop_database(url)
     if not database_exists(url):
         create_database(url)
     engine = create_engine(url)
+
+    for table in country_config["tables"]:
+        table_name = country_config["tables"][table]
+        if table != "other":
+            new_table = type(table_name, (base, ),
+                             {"__tablename__": table_name,
+                              "id": Column(Integer, primary_key=True),
+                              "data": Column(JSONB)})
+        else:
+            table_names = country_config["tables"][table]
+            for table in table_names:
+                new_table = type(table, (base, ),
+                                 {"__tablename__": table,
+                                  "id": Column(Integer, primary_key=True),
+                                  "data": Column(JSONB)})
+
     base.metadata.create_all(engine)
     return True
 
@@ -45,17 +68,30 @@ def import_locations(country_config, engine):
     regions_file = (data_directory + "locations/" +
                     country_config["locations"]["regions"])
     districts_file = (data_directory + "locations/" +
-                    country_config["locations"]["districts"])
+                      country_config["locations"]["districts"])
     clinics_file = (data_directory + "locations/" +
                     country_config["locations"]["clinics"])
     import_regions(regions_file, session, 1)
     import_districts(districts_file, session)
-    import_clinics(clinics_file, session,1)
+    import_clinics(clinics_file, session, 1)
 
-if __name__=="__main__":
 
-    engine = create_engine(DATABASE_URL)
-    Session = sessionmaker(bind=engine)
-    import_locations(country_config, engine)    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=["create-db",
+                                           "import-locations",
+                                           "all"])
+    parser.add_argument("--drop-db", action="store_true")
+    args = parser.parse_args()
 
-    
+    if args.action == "create-db":
+        create_db(DATABASE_URL, model.Base, country_config, drop=args.drop_db)
+    if args.action == "import-locations":
+        engine = create_engine(DATABASE_URL)
+        Session = sessionmaker(bind=engine)
+        import_locations(country_config, engine)
+    if args.action == "all":
+        create_db(DATABASE_URL, model.Base, country_config, drop=args.drop_db)
+        engine = create_engine(DATABASE_URL)
+        Session = sessionmaker(bind=engine)
+        import_locations(country_config, engine)
