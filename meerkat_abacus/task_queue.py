089b6263364243a3dbee9142853107d47dbd2bf2
mrjb <jonathan.s.berry@googlemail.com>
2016-12-06 11:38:52 +0000

    Styled some files.

--- meerkat_abacus/task_queue.py
+++ meerkat_abacus/task_queue.py
@@ -1,15 +1,18 @@
 """
 Celery setup and wraper tasks to periodically update the database.
-
 """
-from celery import Celery
-app = Celery()
-from meerkat_abacus import celeryconfig 
-app.config_from_object(celeryconfig)
 from meerkat_abacus import config, util, data_management
 from celery.signals import worker_ready
 from datetime import datetime
-import requests, logging, traceback
+import requests
+import logging
+import traceback
+from celery import Celery
+from meerkat_abacus import celeryconfig
+
+
+app = Celery()
+app.config_from_object(celeryconfig)
 
 
 # When we start celery we run the set_up_db command
@@ -19,6 +22,7 @@ def set_up_task(**kwargs):
     Start the set_up_db task as soon as workers are ready
     """
     set_up_db.delay()
+
 
 @app.task
 def set_up_db():
@@ -49,10 +53,12 @@ def get_proccess_data(print_progress=False):
     if print_progress:
         print("Finished")
 
+
 @app.task
 def get_new_data_from_s3():
     """Get new data from s3."""
     data_management.get_data_from_s3(config.s3_bucket)
+
 
 @app.task
 def import_new_data():
@@ -61,10 +67,12 @@ def import_new_data():
     """
     return data_management.import_new_data()
 
+
 @app.task
 def add_new_fake_data(to_add):
     """ Add new fake data """
     return data_management.add_new_fake_data(to_add)
+
 
 @app.task
 def new_data_to_codes(restrict_uuids=None):
@@ -75,67 +83,82 @@ def new_data_to_codes(restrict_uuids=None):
                                              restrict_uuids=restrict_uuids
                                              )
 
+
 @app.task
 def send_report_email(report, language, location):
     """Send a report email."""
-    
-    #Important to log properly so we can debug if something goes wrong in deployment.
+
+    # Important to log so we can debug if something goes wrong in deployment.
     pre = "EMAIL " + str(report) + ":  "
-    logging.info( pre + "Trying to send report email."  )
+    logging.warning(pre + "Trying to send report email.")
 
     try:
-        #Authenticate the email sending
+        # Authenticate the email sending
         url = config.auth_root + '/api/login'
-        data = {'username':'report-emails', 'password':config.mailing_key}
+        data = {'username': 'report-emails', 'password': config.mailing_key}
         headers = {'content-type': 'application/json'}
-     
-        logging.info( pre + "Sending authentication request to " + str(url) + " with headers: " + str(headers) )
-        r = requests.request('POST', url, json=data, headers=headers )
-        logging.info( pre + "Received authentication request response: " + str(r) )
 
-        #We need authentication to work, so raise an exception if it doesn't.
+        logging.warning(
+            pre + "Sending authentication request to " +
+            str(url) + " with headers: " + str(headers)
+        )
+        r = requests.request('POST', url, json=data, headers=headers)
+        logging.warning(pre + "Received authentication response: " + str(r))
+
+        # We need authentication to work, so raise an exception if it doesn't.
         if r.status_code != 200:
-            raise Exception( "Authentication request returned not-ok response code: " + str(r.status_code) )
+            raise Exception(
+                "Authentication request returned not-ok response code: " +
+                str(r.status_code)
+            )
 
-        #Create the headers for a properly authenticated request. 
+        # Create the headers for a properly authenticated request.
         token = r.cookies['meerkat_jwt']
-        headers = {**headers, **{'Authorization':'Bearer '+ token }}
+        headers = {**headers, **{'Authorization': 'Bearer ' + token}}
 
-        #Assemble params, we currently send all reports nationally for their default time period. 
+        # Assemble params.
+        # We currently send all reports nationally for the default time period.
         url = config.mailing_root + report + "/" + str(location) + "/"
-        url = url.replace( '/en/', '/'+language+'/' )
+        url = url.replace('/en/', '/' + language + '/')
 
-        #Log the full request so we can debug later if necessary.
-        logging.info( pre + "Sending report email for location: " + 
-                      str(location) + " with language: " + str(language) + 
-                      " using url: " + str(url) + " and headers: " + str(headers) ) 
-     
-        #Make the request and handle the response.
-        r = requests.post( url, json=data, headers=headers )
-        logging.info( pre + "Received email request reponse: " + str(r) )
+        # Log the full request so we can debug later if necessary.
+        logging.info(pre + "Sending report email for location: " +
+                     str(location) + " with language: " +
+                     str(language) + " using url: " + str(url) +
+                     " and headers: " + str(headers))
 
-        #If the response is not a 200 OK, raise an Exception so that we can handle it properly.
+        # Make the request and handle the response.
+        r = requests.post(url, json=data, headers=headers)
+        logging.info(pre + "Received email request reponse: " + str(r))
+
+        # If the response is not a 200 OK, raise an Exception so that we can
+        # handle it properly.
         if r.status_code != 200:
-            raise Exception( "Email request returned not-ok response code: " + str(r.status_code) )
+            raise Exception(
+                "Email request returned not-ok response code: " +
+                str(r.status_code)
+                )
 
-        #Report success
-        logging.info( pre + "Successfully sent " + str(report) + " email." )
+        # Report success
+        logging.info(pre + "Successfully sent " + str(report) + " email.")
 
-    except Exception as e:
-        #Log the exception properly.
-        logging.exception( pre + "Report email request failed." ) 
+    except Exception:
+        # Log the exception properly.
+        logging.exception(pre + "Report email request failed.")
 
-        #Notify the developers that there has been a problem.          
+        # Notify the developers that there has been a problem.
         data = {
             "id": "failed-" + str(report) + "-" + datetime.now().isoformat(),
             "topics": "error-reporting",
             "subject": "FAILED: " + str(report) + " email",
-            "message": ( "<p>Hi <<first_name>> <<last_name>>,</p>" +
-                         "<p>There's been a problem sending the " + str(report) + " report email. " +
-                         "Here's the traceback...</p><p>" + str(traceback.format_exc()) + 
-                         "</p><p>The problem occured at " + datetime.now().isoformat() + " for the " +
-                         "country " + config.country_config.get('country_name', 'ERROR') + 
-                         " with HERMES_DEV == " + str(config.hermes_dev) + ".</p>" +
-                         "<p><b>Hope you can fix it soon!</b></p>" )
+            "message": (
+                "<p>Hi <<first_name>> <<last_name>>,</p>" +
+                "<p>There's been a problem sending the " + str(report) +
+                " report email. " + "Here's the traceback...</p><p>" +
+                str(traceback.format_exc()) + "</p><p>The problem occured " +
+                "at " + datetime.now().isoformat() + " for the country " +
+                config.country_config.get('country_name', 'ERROR') +
+                " with HERMES_DEV == " + str(config.hermes_dev) + ".</p>" +
+                "<p><b>Hope you can fix it soon!</b></p>")
         }
-        util.hermes( '/publish', 'PUT', data ) 
+        util.hermes('/publish', 'PUT', data)
