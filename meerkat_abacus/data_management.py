c00f6a005abc3a0962f83f5f9faff0849ebff1f4
jyri <Jyri Soppela>
2017-02-02 22:35:36 +0000

    python style improvements

--- meerkat_abacus/data_management.py
+++ meerkat_abacus/data_management.py
@@ -770,28 +770,31 @@ def create_links(data_type, input_conditions, table, session, conn):
                 columns.append(bindparam("type", link["name"]).label("type"))
                 columns.append(link_alias.data.label("data_to"))
 
-                #split the semicolon separated join parameters into lists
+                # split the semicolon separated join parameters into lists
                 join_operators = link["method"].split(";")
                 join_operands_from = link["from_column"].split(";")
                 join_operands_to = link["to_column"].split(";")
 
-                #assert that the join parameter lists are equally long
+                # assert that the join parameter lists are equally long
                 assert len(join_operators) == len(join_operands_from)
                 assert len(join_operands_from) == len(join_operands_to)
                  
-                #loop through and handle the lists of join parameters 
+                # loop through and handle the lists of join parameters
                 join_on = []
-                for i in range(0,len(join_operators)):
+                for i in range(0, len(join_operators)):
                     if join_operators[i] == "match":
-                        join_on.append(link_alias.data[join_operands_to[i]].astext == \
+                        join_on.append(link_alias.data[
+                            join_operands_to[i]].astext ==
                             table.data[join_operands_from[i]].astext)
 
                     elif join_operators[i] == "lower_match":
                         join_on.append(func.replace(func.lower(
-                            link_alias.data[join_operands_to[i]].astext), "-", "_") == \
+                            link_alias.data[
+                                join_operands_to[i]].astext), "-", "_") ==
                                 func.replace(
-                                    func.lower(table.data[join_operands_from[i]].astext),
-                                    "-", "_"))
+                                    func.lower(table.data[
+                                        join_operands_from[i]]
+                                               .astext), "-", "_"))
 
                     elif join_operators[i] == "alert_match":
                         join_on.append(link_alias.data[join_operands_to[i]].astext == \
@@ -800,33 +803,33 @@ def create_links(data_type, input_conditions, table, session, conn):
                                 42 - country_config["alert_id_length"],
                                 country_config["alert_id_length"]))
 
-                    #check that the column values used for join are not empty
+                    # check that the column values used for join are not empty
                     conditions.append(
                         link_alias.data[join_operands_to[i]].astext != '')
                     conditions.append(table.data[join_operands_from[i]].astext != '')
 
-                #handle the filter condition
+                # handle the filter condition
                 if link["to_condition"]:
                     column, condition = link["to_condition"].split(":")
                     conditions.append(
                         link_alias.data[column].astext == condition)
 
-                #make sure that the link is not referring to itself
-                conditions.append(from_form.uuid != link_alias.uuid)    
+                # make sure that the link is not referring to itself
+                conditions.append(from_form.uuid != link_alias.uuid)
 
-                #build query from join and filter conditions
+                # build query from join and filter conditions
                 link_query = session.query(*columns).join(
                     link_alias, and_(*join_on)).filter(*conditions)
 
-                #use query to perform insert
+                # use query to perform insert
                 insert = model.Links.__table__.insert().from_select(
                     ("uuid_from", "uuid_to", "type", "data_to"), link_query)
                 conn.execute(insert)
 
-                #split aggregate constraints into a list
+                # split aggregate constraints into a list
                 aggregate_conditions = aggregate_condition.split(';')
 
-                #if the link type has uniqueness constraint, remove non-unique links and circular links
+                # if the link type has uniqueness constraint, remove non-unique links and circular links
                 if 'unique' in aggregate_conditions:
                     dupe_query = session.query(model.Links.uuid_from).\
                                             filter(model.Links.type == link["name"]).\
