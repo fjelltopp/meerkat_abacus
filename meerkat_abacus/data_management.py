5099a4c916a48326f48931030e293e01467a5529
mrjb <jonathan.s.berry@googlemail.com>
2017-02-20 18:03:55 +0000

    Catch more exceptions when setting up db.

--- meerkat_abacus/data_management.py
+++ meerkat_abacus/data_management.py
@@ -41,21 +41,23 @@ def create_db(url, base, drop=False):
     Returns:
         Boolean: True
     """
-    if drop and database_exists(url):
-        drop_database(url)
-    if not database_exists(url):
-        counter = 0
-        while counter<5:
-            try:
+    counter = 0
+    while counter < 5:
+        try:
+            if drop and database_exists(url):
+                print('Dropping database.')
+                drop_database(url)
+            if not database_exists(url):
                 print('Creating database.')
                 create_database(url)
                 break
-            except exc.OperationalError as e:
-                print('There was an error connecting to the db.')
-                print(e)
-                print('Trying again in 5 seconds...')
-                time.sleep(5)
-                counter = counter + 1
+
+        except exc.OperationalError as e:
+            print('There was an error connecting to the db.')
+            print(e)
+            print('Trying again in 5 seconds...')
+            time.sleep(5)
+            counter = counter + 1
 
     engine = create_engine(url)
     connection = engine.connect()
