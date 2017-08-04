5d4d5bfd49d59ffe9acd62054f3de764af399bf7
Tomasz Sabała <sabala.tomek@gmail.com>
2017-08-04 16:12:24 +0200

    Event dates are read from meerkat properties.

--- meerkat_abacus/country_config/demo_dhis2.py
+++ meerkat_abacus/country_config/demo_dhis2.py
@@ -2,13 +2,19 @@ dhis2_config = {
     "url": "http://54.76.53.0:8080",
     "api_resource": "/api/26/",
     "credentials": ('admin', 'district'),
-    "headers": {"Content-Type": "application/json", "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="},
+    "headers": {
+        "Content-Type": "application/json",
+        "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="
+    },
     "forms": [
         {
             "name": "demo_case",
-            "date": "pt./visit_date",
+            "event_date": "pt./visit_date",
+            "completed_date": "end",
             "organisation_unit_id": "wZxJHG0MUNz",
-            "program_id": "ZU7Z7ouwbba"
+            "program_id": "ZU7Z7ouwbba",
+            "stored_by": "admin",
+            "status": "COMPLETED"
         },
         {
             "name": "demo_alert",
