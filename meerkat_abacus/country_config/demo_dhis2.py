ad6a4cca55c5cce4eee40dd2a4c377eeeb966158
Tomasz Sabała <sabala.tomek@gmail.com>
2017-08-16 17:13:26 +0200

    Program is not created and updated dynamically.

--- meerkat_abacus/country_config/demo_dhis2.py
+++ meerkat_abacus/country_config/demo_dhis2.py
@@ -1,19 +1,19 @@
 dhis2_config = {
-    "url": "http://localhost:8085",
-    # "url": "http://54.76.53.0:8080",
+    "url": "http://54.76.53.0:8080",
     "api_resource": "/api/26/",
     "credentials": ('admin', 'district'),
     "headers": {
         "Content-Type": "application/json",
         "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="
     },
-    "country_id": "k83FUJTHUel",
+    # "country_id": "k83FUJTHUel",
+    "country_id": "EebWN0q7GpT", # Null Island
     "forms": [
         {
             "name": "demo_case",
             "event_date": "pt./visit_date",
             "completed_date": "end",
-            "program_id": "fgrH0jPDNEP",
+            # "program_id": "fgrH0jPDNEP",
             "stored_by": "admin",
             "status": "COMPLETED"
         },
