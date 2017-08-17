bee36eade3affe22458cb00f20eba47aefe30986
Tomasz Sabała <sabala.tomek@gmail.com>
2017-08-17 12:41:40 +0200

    Added a logging wraper to log not OK responses.
    Preparing code for tests.

--- meerkat_abacus/country_config/demo_dhis2.py
+++ meerkat_abacus/country_config/demo_dhis2.py
@@ -1,20 +1,20 @@
 dhis2_config = {
     "url": "http://54.76.53.0:8080",
-    "api_resource": "/api/26/",
+    "apiResource": "/api/26/",
     "credentials": ('admin', 'district'),
     "headers": {
         "Content-Type": "application/json",
         "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="
     },
+    "loggingLevel": "DEBUG",
     # "country_id": "k83FUJTHUel",
-    "country_id": "EebWN0q7GpT", # Null Island
+    "countryId": "EebWN0q7GpT", # Null Island
     "forms": [
         {
             "name": "demo_case",
             "event_date": "pt./visit_date",
             "completed_date": "end",
-            # "program_id": "fgrH0jPDNEP",
-            "stored_by": "admin",
+            # "programId": "fgrH0jPDNEP",
             "status": "COMPLETED"
         },
         {
