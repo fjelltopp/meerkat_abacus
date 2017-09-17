f4ed243aa8e24af2d0a0b09e41888095db8bcd93
jyri <soppela.jyri@gmail.com>
2017-09-17 10:03:22 +0300

    reactivate Aggragete submissions

--- meerkat_abacus/tasks.py
+++ meerkat_abacus/tasks.py
@@ -139,7 +139,7 @@ def add_fake_data(N=10, interval_next=None, dates_is_now=False, internal_fake_da
                          "data": row})
             elif aggregate_url:
                 logging.info("Submitting fake data for form {0} to Aggregate".format(form))
-                # util.submit_data_to_aggregate(row, form, config)
+                util.submit_data_to_aggregate(row, form, config)
     if interval_next:
         add_fake_data.apply_async(countdown=interval_next,
                                   kwargs={"interval_next": interval_next,
