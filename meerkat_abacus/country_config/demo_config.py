d1e2310b0f0fb309efa754c07f9d70fb15333161
Jonathan Berry <jonathan.s.berry@googlemail.com>
2017-10-19 14:15:48 +0300

    Add the demo codes needed to report submitted date.

--- meerkat_abacus/country_config/demo_config.py
+++ meerkat_abacus/country_config/demo_config.py
@@ -137,10 +137,10 @@ country_config = {
     },
     "alert_data": {"demo_case": {"age": "pt1./age", "gender": "pt1./gender"}},
     "alert_id_length": 6,
-    "alert_text_content": ['reason', 'date', 'clinic', 'region', 'patient', 'gender', 'age', 'id', 'received' ],
+    "alert_text_content": ['reason', 'date', 'clinic', 'region', 'patient', 'gender', 'age', 'id', 'submitted', 'received', 'id' ],
     "alert_sms_content": ['reason', 'date', 'clinic', 'region', 'gender', 'age', 'id'],
     "alert_email_content": [
-        'reason', 'date', 'clinic', 'region', 'breaker', 'patient', 'gender', 'age', 'breaker', 'id', 'received'
+        'reason', 'date', 'clinic', 'region', 'breaker', 'patient', 'gender', 'age', 'breaker', 'submitted', 'received', 'id'
     ],
     "messaging_start_date": datetime.datetime(2016, 2, 15),
     "messaging_topic_prefix": "null",
