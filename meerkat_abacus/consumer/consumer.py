c4df915bab81d1c150521cdd547eb2c522f6bc18
Tomasz Sabała <sabala.tomek@gmail.com>
2018-03-16 15:20:20 +0100

    Consumer startup while loop refactor.

--- meerkat_abacus/consumer/consumer.py
+++ meerkat_abacus/consumer/consumer.py
@@ -17,9 +17,10 @@ config = get_config()
 logging.getLogger().setLevel(logging.INFO)
 
 app = Celery()
-#app.purge()
+# app.purge()
 app.config_from_object(celeryconfig)
 session, engine = database_setup.set_up_database(False, True, config)
+
 
 @backoff.on_exception(backoff.expo,
                       (celery.exceptions.TimeoutError,
@@ -30,6 +31,8 @@ def wait_for_celery_runner():
     test_task = test_up.delay()
     result = test_task.get(timeout=1)
     return result
+
+
 wait_for_celery_runner()
 # Initial Setup
 
@@ -48,7 +51,7 @@ elif config.initial_data_source == "FAKE_DATA":
     create_fake_data.create_fake_data(session,
                                       config,
                                       write_to="file")
- 
+
 elif config.initial_data_source in ["AWS_RDS", "LOCAL_RDS"]:
     get_function = util.get_data_from_rds_persistent_storage
 else:
@@ -57,26 +60,47 @@ else:
 get_data.read_stationary_data(get_function, config)
 database_setup.logg_tables(config.country_config["tables"], engine)
 
+
 # Real time
 
-while True:
+def real_time_s3():
+    get_data.download_data_from_s3(config)
+    get_data.read_stationary_data(util.read_csv_file, config)
+    time.sleep(config.data_stream_interval)
+
+
+def real_time_fake():
+    logging.info("Sending fake data")
+    if config.fake_data_generation == "INTERNAL":
+        new_data = []
+        for form in config.country_config["tables"]:
+            data = create_fake_data.get_new_fake_data(form=form,
+                                                      session=session,
+                                                      N=10,
+                                                      param_config=config,
+                                                      dates_is_now=True)
+            new_data = [{"form": form, "data": d[0]} for d in data]
+        app.send_task('process_data', [new_data])
+    else:
+        raise NotImplementedError("Not yet implemented")
+    logging.info("Sleeping")
+    time.sleep(config.fake_data_interval)
+
+
+def main():
+    def run():
+        pass
+
     if config.stream_data_source == "AWS_S3":
-        get_data.download_data_from_s3(config)
-        get_data.read_stationary_data(util.read_csv_file, config)
-        time.sleep(config.data_stream_interval)
+        run = real_time_s3
     elif config.stream_data_source == "FAKE_DATA":
-        logging.info("Sending fake data")
-        if config.fake_data_generation == "INTERNAL":
-            new_data = []
-            for form in config.country_config["tables"]:
-                data = create_fake_data.get_new_fake_data(form=form,
-                                                          session=session,
-                                                          N=10,
-                                                          param_config=config,
-                                                          dates_is_now=True)
-                new_data = [{"form": form, "data": d[0]} for d in data]
-            app.send_task('process_data', [new_data])
-        else:
-            raise NotImplementedError("Not yet implemented")
-        logging.info("Sleeping")
-        time.sleep(config.fake_data_interval)
+        run = real_time_fake
+    else:
+        RuntimeError("Unsupported data source.")
+
+    while True:
+        run()
+
+
+if __name__ == '__main__':
+    main()
