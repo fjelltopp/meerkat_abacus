06812a5f21a0eebfa835046c8e0ae3f9d9a14875
Tomasz Sabała <sabala.tomek@gmail.com>
2018-03-16 16:19:06 +0100

    Polish up logging and basic python stuff.

--- meerkat_abacus/consumer/get_data.py
+++ meerkat_abacus/consumer/get_data.py
@@ -9,22 +9,20 @@ def read_stationary_data(get_function, param_config, N_send_to_task=1000):
     Read stationary data using the get_function to determine the source
     """
 
-    for form in param_config.country_config["tables"]:
-        i = 0
-        logging.info(form)
+    for form_name in param_config.country_config["tables"]:
+        logging.info(f"Start processing data for form {form_name}")
         data = []
-        for element in get_function(form, param_config=param_config):
-            data.append({"form": form,
+        for i, element in enumerate(get_function(form_name, param_config=param_config)):
+            data.append({"form": form_name,
                          "data": dict(element)})
-            if len(data) == N_send_to_task:
+            if i % N_send_to_task == 0:
+                logging.info(f"Processed {i} records")
                 processing_tasks.process_data.delay(data)
                 data = []
-            i += 1
-            if i % 1000 == 0:
-                logging.info(i)
         if data:
+            logging.info("Cleaning up data buffer.")
             processing_tasks.process_data.delay(data)
-
+        logging.info("Finished processing data.")
 
 def download_data_from_s3(config):
     """
@@ -36,10 +34,11 @@ def download_data_from_s3(config):
        bucket: bucket_name
     """
     s3 = boto3.resource('s3')
-    for form in config.country_config["tables"]:
-        file_name = form + ".csv"
-        s3.meta.client.download_file(config.s3_bucket, "data/" + file_name,
-                                     config.data_directory + file_name)
+    for form_name in config.country_config["tables"]:
+        file_name = form_name + ".csv"
+        s3_key = "data/" + file_name
+        destination_path = config.data_directory + file_name
+        s3.meta.client.download_file(config.s3_bucket, s3_key, destination_path)
 
         
 
